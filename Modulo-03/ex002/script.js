240c01aa5b4829641e920ccf5f36c0e28a10e37f
HenriqueMafort <hmafort@gmail.com>
2022-08-15 18:04:33 -0300

    Finalizando dia de trabalho

---
+++ Modulo-03/ex002/script.js
@@ -0,0 +1,7 @@
+/*var nome = window.prompt('Digite seu nome: ')
+var idade = window.prompt('Qual é sua idade')
+if(idade < 18){
+    alert('Desculpe não podemos mostrar o conteudo da pagina devido a sua idade.')
+}else if ( idade >= 18){
+window.alert(`Seja Bem vindo ${nome}! `)
+}*/
